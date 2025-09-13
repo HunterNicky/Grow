750abcf301ca144756754fa6149ded155038a858
nickyzaguine <nickyzaguine@gmail.com>
2025-09-13 10:09:18 -0300

    Update boost.cmake

--- cmake/dependencies/boost.cmake
+++ cmake/dependencies/boost.cmake
@@ -5,8 +5,11 @@ set(BUILD_TESTING OFF CACHE BOOL "Disable Boost tests" FORCE)
 add_compile_definitions(
 	_CRT_SECURE_NO_WARNINGS
 	_SILENCE_CXX23_DENORM_DEPRECATION_WARNING
-    _WIN32_WINNT=0x0A00
 )
+
+if(WIN32)
+	add_compile_definitions(_WIN32_WINNT=0x0A00)
+endif()
 
 add_subdirectory(${THIRD_PARTY_DIR}/boost)
 
