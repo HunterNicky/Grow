5b2e7577ac507e84e5f50ddd8a4b32cc9af12fd0
nickyzaguine <nickyzaguine@gmail.com>
2025-09-13 10:32:56 -0300

    Update boost.cmake

--- cmake/dependencies/boost.cmake
+++ cmake/dependencies/boost.cmake
@@ -3,20 +3,8 @@ set(BoostRoot ${THIRD_PARTY_DIR}/boost)
 set(BUILD_TESTING OFF CACHE BOOL "Disable Boost tests" FORCE)
 
 add_compile_definitions(
-	_CRT_SECURE_NO_WARNINGS
-	_SILENCE_CXX23_DENORM_DEPRECATION_WARNING
+  _CRT_SECURE_NO_WARNINGS
+  _SILENCE_CXX23_DENORM_DEPRECATION_WARNING
 )
 
-if(WIN32)
-	add_compile_definitions(_WIN32_WINNT=0x0A00)
-endif()
-
 add_subdirectory(${THIRD_PARTY_DIR}/boost)
-
-if(MSVC)
-	foreach(_cobalt_tgt IN ITEMS boost_cobalt boost_cobalt_io boost_cobalt_io_ssl)
-		if (TARGET ${_cobalt_tgt})
-			target_compile_definitions(${_cobalt_tgt} PUBLIC _WIN32_WINNT=0x0603)
-		endif()
-	endforeach()
-endif()
