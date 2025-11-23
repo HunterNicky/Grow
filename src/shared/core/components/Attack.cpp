7e0508bef0b51ea255993eb5dbc900c137670067
Gustavo Aguilar <gusagui2005@gmail.com>
2025-11-22 23:33:34 -0300

    fix: remove initialize attack

--- src/shared/core/components/Attack.cpp
+++ src/shared/core/components/Attack.cpp
@@ -3,7 +3,7 @@
 #include "chroma/shared/core/components/Component.h"
 
 namespace chroma::shared::core::component {
-Attack::Attack() : { type_ = ComponentType::ATTACK; }
+Attack::Attack() { type_ = ComponentType::ATTACK; }
 Attack::Attack(bool is_attacking) : is_attacking_(is_attacking) { type_ = ComponentType::ATTACK; }
 bool Attack::IsAttacking() const { return is_attacking_; }
 
